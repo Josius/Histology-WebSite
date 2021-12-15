b0b2677118cef31a7e729c37769973a679ccd264
Josildus L'Amarus <josius.amaro@gmail.com>
2021-12-15 18:18:05 -0300

    zoomify aparentemente funcionando perfeitamente, ou em outras palavras, chupa flask! :D

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-engine = create_engine('postgresql+psycopg2://histadmin:histadmin@localhost:5432/histdb')
+engine = create_engine('postgresql+psycopg2://postgres:Josimar@localhost:5432/histdb')
 DBSession = sessionmaker()
 DBSession.configure(bind=engine)
 db_session = DBSession()
