07bb4b2d5709bd9567bb5ae726af6f300eecf8ff
Josildus L'Amarus <josius.amaro@gmail.com>
2022-03-24 14:13:45 -0300

    alterações em .html, .css e imagens

--- db.py
+++ db.py
@@ -3,9 +3,6 @@ from sqlalchemy.orm import sessionmaker
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-#engine = create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/histdb')
-# engine = create_engine('postgresql+psycopg2://postgres:z3-L7*Te@localhost:5432/histdb')
-# engine = create_engine('postgresql+psycopg2://postgres:Josimar@localhost:5432/histdb')
 engine = create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/histdb')
 
 DBSession = sessionmaker()
