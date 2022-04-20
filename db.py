23861d5974b28f35b665e7c1f6768c6fb8894963
PSemcovici <pedrosemcovici@outlook.com>
2022-04-20 17:00:17 -0300

    Mais alteracoes no menu retratil

--- db.py
+++ db.py
@@ -3,8 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-engine = create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/histdb')
-# engine = create_engine('postgresql+psycopg2://postgres:virada50@localhost:5432/histdb')
+engine = create_engine('postgresql+psycopg2://postgres:virada50@localhost:5432/histdb')
 
 DBSession = sessionmaker()
 DBSession.configure(bind=engine)
