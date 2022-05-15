1040585905b6c1e9ffcc956e3a4150d50ac2afc5
semcovici <pedrosemcovici@outlook.com>
2022-05-15 01:10:44 -0300

    alguns ajustes

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-engine = create_engine('postgresql+psycopg2://postgres:virada50@localhost:5432/histdb')
+engine = create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/histdb')
 DBSession = sessionmaker()
 DBSession.configure(bind=engine)
 db_session = DBSession()
